ca444ffc03b37ef894d784dc88c470ffc58c8436
Gregorio Alvarez <galvarez@cbpi.com.mx>
2024-01-30 22:58:28 -0400

    [feature] Add more Vector operations and use it to calculate average velocity as example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,58 @@
 import Vector from './modules/vector';
 
+function averageVelocity(
+  origin: Vector,
+  destination: Vector,
+  deltaTime: number,
+) {
+  return Vector.substract(destination, origin).scalarMultiplication(
+    1 / deltaTime,
+  );
+}
+
 /**
  * Main function. App entry point
  */
 (async function (): Promise<void> {
-  const a = new Vector([0, 1, 2]);
-  const scalar = 8;
+  const i = new Vector([1, 0, 0]);
+  const j = new Vector([0, 1, 0]);
+  const k = new Vector([0, 0, 1]);
 
-  const scaledVector = Vector.scalarMultiplication(a, scalar);
+  // console.log('i = ', i);
+  // console.log('j = ', j);
+  // console.log('k =', k);
+  // console.log('||i|| =', i.magnitude());
+  // console.log('||j|| =', j.magnitude());
+  // console.log('||k|| =', k.magnitude());
+  // console.log('i.j =', Vector.dotProduct(i, j));
+  // console.log('i+j =', Vector.add(i, j));
+  // console.log('i-y =', Vector.substract(i, j));
 
-  console.log('Dimension', a.dimension);
+  // console.log(
+  //   'a.b =',
+  //   Vector.dotProduct(new Vector([1, 2]), new Vector([2, 3])),
+  // );
 
-  console.log('Vector', a);
-  console.log('Scalar', scalar);
-  console.log('Scaled Vector', a.scalarMultiplication(scalar));
-  console.log('Scaled Vector Static', scaledVector);
+  // console.log('cosine angle of a.b =', Vector.cosineOnVectors(i, j));
+  // console.log(
+  //   'angle of a.b =',
+  //   Vector.angle(i, j),
+  //   '=',
+  //   `${Number(Vector.angle(i, j) / Math.PI).toFixed(3)}π radians`,
+  // );
+
+  const initialPosition = new Vector([0, 0, 0]); // m
+
+  const finalPosition = new Vector([10, 20, 0]); // m
+
+  const deltaTime = 20; // secs
+
+  console.log(
+    'Average velocity',
+    averageVelocity(initialPosition, finalPosition, deltaTime),
+    'm/s',
+    '=',
+    averageVelocity(initialPosition, finalPosition, deltaTime).magnitude(),
+    'm/s',
+  );
 })();
--- src/modules/vector.ts
+++ src/modules/vector.ts
@@ -26,14 +26,85 @@ export default class Vector extends Array<number> {
    *
    * @return {Vector} a new vector with the result
    */
-  add(otherVector: Array<number>): Vector {
-    const newVectorLength = Math.max(otherVector.length, this.length);
+  add(otherVector: Vector): Vector {
+    return Vector.add(this, otherVector);
+  }
 
-    const currentVectorValues = this || [];
+  /**
+   * Substract current vector to another
+   *
+   * @param {Vector} otherVector
+   *
+   * @return {Vector} a new vector with the result
+   */
+  substract(otherVector: Vector): Vector {
+    return Vector.substract(this, otherVector);
+  }
 
-    const newValues = new Array(newVectorLength)
+  /**
+   * Multiply scalar to current vector
+   *
+   * @param {number} scalar
+   *
+   * @return {Vector} resulting vector
+   */
+  scalarMultiplication(scalar: number): Vector {
+    return Vector.scalarMultiplication(this, scalar);
+  }
+
+  /**
+   * Calculate the magnitude or length of a vector
+   */
+  magnitude(): number {
+    const components: number[] = Array.from(this);
+
+    const squaredModule = components.reduce(
+      (accumulator = 0, component) => accumulator + Math.pow(component, 2),
+      0,
+    );
+
+    const module = Math.sqrt(squaredModule);
+
+    return module;
+  }
+
+  /**
+   * dotProcut of current vector with provided one
+   *
+   * @param {Vector} vector
+   *
+   * @return {number} resulting product
+   */
+  dotProduct(vector: Vector): number {
+    return Vector.dotProduct(this, vector);
+  }
+
+  /**
+   * Calculate the cosine of the angle between current vector and provided vector
+   *
+   * @param {Vector} vector
+   *
+   * @return {number}
+   */
+  cosineOnVector(vector: Vector): number {
+    return Vector.cosineOnVectors(this, vector);
+  }
+
+  /**
+   * Add current vector to another
+   *
+   * @param {Vector} otherVector
+   *
+   * @return {Vector} a new vector with the result
+   */
+  static add(vectorA: Vector, vectorB: Vector): Vector {
+    const newVectorDimension = Math.max(vectorA.dimension, vectorB.dimension);
+
+    const currentVectorValues = Array.from(vectorA);
+
+    const newValues = new Array(newVectorDimension)
       .fill(0)
-      .map((e, i) => (currentVectorValues?.[i] || 0) + (otherVector[i] || 0));
+      .map((e, i) => (currentVectorValues?.[i] || 0) + (vectorB?.[i] || 0));
 
     const newVect = new Vector(newValues);
 
@@ -47,35 +118,18 @@ export default class Vector extends Array<number> {
    *
    * @return {Vector} a new vector with the result
    */
-  substract(otherVector: Array<number>): Vector {
-    const newVectorLength = Math.max(otherVector.length, this.length);
+  static substract(vectorA: Vector, vectorB: Vector): Vector {
+    const newVectorDimension = Math.max(vectorA.dimension, vectorB.dimension);
 
-    const currentVectorValues = this || [];
+    const currentVectorValues = Array.from(vectorA);
 
-    const newValues = new Array(newVectorLength)
+    const newValues = new Array(newVectorDimension)
       .fill(0)
-      .map((e, i) => (currentVectorValues?.[i] || 0) - (otherVector[i] || 0));
+      .map((e, i) => (currentVectorValues?.[i] || 0) - (vectorB?.[i] || 0));
 
     const newVect = new Vector(newValues);
 
     return newVect;
-  }
-
-  /**
-   * Multiply scalar to current vector
-   *
-   * @param {number} scalar
-   *
-   * @return {Vector} resulting vector
-   */
-  scalarMultiplication(scalar: number): Vector {
-    const newValues: number[] = Array.from(this).map(
-      (element) => element * scalar,
-    );
-
-    const newVector = new Vector(newValues);
-
-    return newVector;
   }
 
   /**
@@ -94,5 +148,61 @@ export default class Vector extends Array<number> {
     const newVector = new Vector(newValues);
 
     return newVector;
+  }
+
+  /**
+   * dotProcut for provided vectors
+   *
+   * @param {Vector} vectorA
+   * @param {Vector} vectorB
+   *
+   * @return {number} resulting product
+   */
+  static dotProduct(vectorA: Vector, vectorB: Vector): number {
+    if (vectorA.dimension !== vectorB.dimension) {
+      throw new Error(
+        'Vectors must have the same dimension to apply dot product',
+      );
+    }
+
+    const dotProductResult: number = Array.from(vectorA).reduce(
+      (accumulator, component, index) =>
+        accumulator + component * (vectorB?.[index] || 0),
+      0,
+    );
+
+    return dotProductResult;
+  }
+
+  /**
+   * Calculate the cosine of the angle between two provided vectors
+   *
+   * @param {Vector} vectorA
+   * @param {Vector} vectorB
+   *
+   * @return {number} cosine of angle between vectors
+   */
+  static cosineOnVectors(vectorA: Vector, vectorB: Vector): number {
+    const dotProduct = this.dotProduct(vectorA, vectorB);
+    const magnitudeA = vectorA.magnitude();
+    const magnitudeB = vectorB.magnitude();
+
+    const cosine = Number(dotProduct / (magnitudeA * magnitudeB));
+
+    return cosine;
+  }
+
+  /**
+   * Returns the angle of two vectors in radians
+   *
+   * @param {Vector} vectorA
+   * @param {Vector} vectorB
+   *
+   * @return {number}
+   */
+  static angle(vectorA: Vector, vectorB: Vector): number {
+    const cosine = vectorA.cosineOnVector(vectorB);
+
+    return Math.acos(cosine);
   }
 }
