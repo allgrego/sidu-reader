88fc148cef069c6494d8265e22ba684735347114
Gregorio Alvarez <galvarez@cbpi.com.mx>
2024-03-06 10:02:41 -0400

    [feature] Removed repeated consignees and cleaner CLI UI

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import fs from 'fs/promises';
 import countries from 'i18n-iso-countries';
 import xlsx from 'node-xlsx';
 import path from 'node:path';
@@ -20,9 +21,14 @@ const XLSX_FILE_PATH = 'files/CMAI-0ER9SN1MA.xlsx';
  */
 async function main(): Promise<void> {
   try {
+    console.log(
+      '- - - - - - - - - - - - - - - - - - - -  SIDU-READER - - - - - - - - - - - - - - - - - - - - ',
+      '\n',
+    );
+
     const filePath = path.resolve(XLSX_FILE_PATH);
 
-    console.log('filePath', filePath);
+    console.log('* Data obtained from:'.padEnd(30, ' '), filePath, '\n');
 
     // Parse a file
     const workSheetsFromFile = xlsx.parse(filePath);
@@ -188,7 +194,7 @@ async function main(): Promise<void> {
 
     const rawOperationsList = Object.values(operationsRecord);
 
-    const operations: Operation[] = rawOperationsList.map(
+    const cleanOperations: Operation[] = rawOperationsList.map(
       ({
         blNumber,
         cargoDescription,
@@ -200,6 +206,7 @@ async function main(): Promise<void> {
         const description = cargoDescription
           .join(' ')
           .replace('S/M', '')
+          .replace('s/m', '')
           .replace('PAQUETE', '')
           .trim();
 
@@ -258,7 +265,103 @@ async function main(): Promise<void> {
       },
     );
 
-    console.log('operations', operations);
+    const consignees: string[] = [];
+
+    const repeatedConsignees: string[] = [];
+
+    // Remove the operations of repeated consignees
+    const operations = cleanOperations.filter(({ consignee }) => {
+      if (consignees.includes(consignee)) {
+        repeatedConsignees.push(consignee);
+        return false;
+      }
+
+      consignees.push(consignee);
+      return true;
+    });
+
+    const headers: string[] = [
+      'consignee',
+      'totalContainers',
+      'location',
+      'originPort',
+      'originCountry',
+      'destinationPort',
+      'destinationCountry',
+      'cargoAmount',
+      'cargoType',
+      'cargoDescription',
+      'blNumber',
+      'pages',
+      'opType',
+    ];
+
+    const data: string[][] = [
+      headers,
+      ...operations.map(
+        ({
+          consignee,
+          totalContainers,
+          location,
+          originPort,
+          originCountry,
+          destinationPort,
+          destinationCountry,
+          cargoAmount,
+          cargoType,
+          cargoDescription,
+          blNumber,
+          pages,
+          opType,
+        }) => {
+          return [
+            consignee,
+            totalContainers,
+            location,
+            originPort,
+            originCountry,
+            destinationPort,
+            destinationCountry,
+            cargoAmount,
+            cargoType,
+            cargoDescription,
+            blNumber,
+            pages,
+            opType,
+          ];
+        },
+      ),
+    ];
+
+    const buffer = xlsx.build([{ name: 'data', data: data, options: {} }]);
+
+    const newFilePath = path.resolve('files/new.xlsx');
+
+    await fs.writeFile(newFilePath, buffer);
+
+    // console.log('operations', operations);
+    // console.log('Included consignees', Array.from(new Set(consignees)));
+    // console.log('Repeated consignees', Array.from(new Set(repeatedConsignees)));
+
+    console.log(
+      '- - - - - - - - - - - - - - - - - - - -  STATS - - - - - - - - - - - - - - - - - - - - ',
+      '\n',
+    );
+
+    console.log('* Total operations:'.padEnd(32, ' '), cleanOperations.length);
+    console.log(
+      '* Valid operations included'.padEnd(32, ' '),
+      operations.length,
+      '/',
+      cleanOperations.length,
+    );
+    console.log(
+      '* Repeated operations removed:'.padEnd(32, ' '),
+      repeatedConsignees.length,
+      '/',
+      cleanOperations.length,
+    );
+    console.log('* New File created at'.padEnd(32, ' '), newFilePath);
   } catch (error) {
     console.error('Failure', error);
     throw error;
